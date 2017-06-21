b7fe74e6118d82c7e83453f99108d58639f7f6ec
sfmajors373 <sfmajors373@gmail.com>
2017-06-21 11:00:50 -0700

    trying to get the two files to connect

---
+++ dom.js
@@ -0,0 +1,12 @@
+var aboutme = document.getElementsByTagName('h1');
+var favorites = document.getElementById("favorites");
+var nickname = document.getElementById("nickname");
+var hometown = document.getElementById("hometown");
+var body = document.getElementByTagName("BODY");
+
+body.style.backgroundColor = "red";
+body.style.fontFamily = "sans-serif";
+aboutme.style.backgroundColor = "blue";
+favorites.innerHTML = "many things";
+nickname.innerHTML = "Sarah";
+hometown.innerHTML = "Pittsburgh";
